178f3e78b5b6a3f254883503dcd951e43e6b44bf
kajii hiroto <e1b19019@st.oit.ac.jp>
2022-08-05 01:34:59 +0900

    文字入力の作成

--- src/main/java/com/example/demo/contoroller/mainContoroller.java
+++ src/main/java/com/example/demo/contoroller/mainContoroller.java
@@ -1,23 +1,35 @@
 package com.example.demo.contoroller;
 
 import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 //import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.player;
 
 @Controller
 @RequestMapping("/test")
 public class mainContoroller{
-    @GetMapping("test1")
-    public String getMethodName(ModelMap model, Principal prin) {
-        return "test.html";
+
+    @GetMapping("/form")
+    public String readForm(@ModelAttribute player player) {
+      return "test.html";
+    }
+
+    @PostMapping("/form")
+    public String create(@ModelAttribute player player) {
+      return "test2.html";
     }
 }
-
